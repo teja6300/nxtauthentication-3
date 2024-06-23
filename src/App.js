ef4980c69836ccfef4221f4f60661494500d3860
teja6300 <218w1aa1239@vrsec.ac.in>
2024-06-23 05:26:44 +0000

    auth -3

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
+import LoginForm from './components/LoginForm'
+import Home from './components/Home'
+import Products from './components/Products'
+import Cart from './components/Cart'
+import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+import './App.css'
+
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/login" component={LoginForm} />
+      <ProtectedRoute exact path="/" component={Home} />
+      <ProtectedRoute exact path="/products" component={Products} />
+      <ProtectedRoute exact path="/cart" component={Cart} />
+      <ProtectedRoute component={NotFound} />
+    </Switch>
+  </BrowserRouter>
+)
+
+export default App
